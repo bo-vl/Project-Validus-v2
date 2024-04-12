b9e78815971c3023d10ff3d677ec66ee7cfa584e
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2024-04-12 16:41:35 +0200

    update
    + added some functions checks
    + added function callable fix

--- utils/check.lua
+++ utils/check.lua
@@ -1,1 +1,34 @@
-local func = 
+local success, func = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/Bovanlaarhoven/Project-Validus-v2/main/utils/func.lua"))()
+end)
+
+if success then
+    local testVector = Vector3.new(10, 10, 10)
+    local screenPos, onScreen = GetScreenPosition(testVector)
+    local testPlayer = plrs:GetPlayers()[2]
+    print(testPlayer.Name) 
+    print("Screen position:", screenPos)
+    print("On screen:", onScreen)
+    
+    print("Is player alive:", func.IsAlive(testPlayer))
+    
+    print("Is player on different team:", func.TeamCheck(testPlayer))
+    
+    local mousePos = GetMousePosition()
+    print("Mouse position:", mousePos)
+    
+    local gun = func.GetGun(testPlayer)
+    print("Player's gun:", gun)
+    
+    print("Is player visible:", func.IsPlayerVisible(testPlayer))
+    
+    local testChance = 50
+    print("Did hit chance succeed:", func.HitChance(testChance))
+    
+    local testOrigin = Vector3.new(0, 0, 0)
+    local testPos = Vector3.new(10, 10, 10)
+    print("Direction vector:", func.Direction(testOrigin, testPos))
+    return func
+else
+    print("Error loading func.lua:", func)
+end
--- utils/func.lua
+++ utils/func.lua
@@ -5,38 +5,40 @@ local GetPlayers = plrs.GetPlayers
 local GetPartsObscuringTarget = Camera.GetPartsObscuringTarget
 local mouse = lplr:GetMouse()
 
-local GetScreenPosition = function(Vector)
+local functions = {}
+
+functions.GetScreenPosition = function(Vector)
     local Vec3 = Camera:WorldToViewportPoint(Vector)
     local OnScreen = Vec3.Z > 0
     return Vector2.new(Vec3.X, Vec3.Y), OnScreen
 end
 
-local IsTool = function(Tool)
+functions.IsTool = function(Tool)
     return Tool:IsA("Tool")
 end
 
-local IsAlive = function(Plr)
+functions.IsAlive = function(Plr)
     return Plr.Character and Plr.Character:FindFirstChild("Humanoid") and Plr.Character.Humanoid.Health > 0
 end
 
-local TeamCheck = function(Plr)
+functions.TeamCheck = function(Plr)
     return Plr.Team ~= lplr.Team
 end
 
-local GetMousePosition = function()
+functions.GetMousePosition = function()
     return Vector2.new(mouse.X, mouse.Y)
 end
 
-local GetGun = function(Plr)
+functions.GetGun = function(Plr)
     local Character = Plr.Character
     if not Character then return end
     for _,v in ipairs(Character:GetChildren()) do
-        if not IsTool(v) then return end
+        if not functions.IsTool(v) then return end
         return v
     end
 end
 
-local IsPlayerVisible = function(Player)
+functions.IsPlayerVisible = function(Player)
     local PlayerCharacter = Player.Character
     local LocalPlayerCharacter = lplr.Character
     
@@ -57,13 +59,15 @@ local IsPlayerVisible = function(Player)
     return #ObscuringObjects == 0
 end
 
-local HitChance = function(Percentage)
+functions.HitChance = function(Percentage)
     Percentage = math.floor(Percentage)
     local chance = math.floor(Random.new().NextNumber(Random.new(),0,1) * 100) / 100
 
     return chance <= Percentage / 100
 end
 
-local Direction = function(Origin, Pos)
+functions.Direction = function(Origin, Pos)
     return (Pos - Origin).unit
 end
+
+return functions
