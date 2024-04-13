6e533406a49abb4aee17d0b91622bcea4513e675
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2024-04-13 23:57:54 +0200

    Update func.lua

--- utils/func.lua
+++ utils/func.lua
@@ -32,7 +32,7 @@ functions.GetMousePosition = function()
 end
 
 functions.GetGun = function(Plr)
-    local Character = plr.Character
+    local Character = lplr.Character
     if not Character then return end
     for _,v in ipairs(Character:GetChildren()) do
         if functions.IsTool(v) then
